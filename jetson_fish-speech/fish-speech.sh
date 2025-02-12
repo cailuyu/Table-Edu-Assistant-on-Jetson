794df8003370dfafcaaecf2833fb32f657c9a410
cailuyu <cailuyu@hotmail.com>
2025-02-12 14:38:52 +0800

    add fish-speech

---
+++ jetson_fish-speech/fish-speech.sh
@@ -0,0 +1,1 @@
+python -m tools.api_server     --listen 0.0.0.0:8080     --llama-checkpoint-path "checkpoints/fish-speech-1.5"     --decoder-checkpoint-path "checkpoints/fish-speech-1.5/firefly-gan-vq-fsq-8x1024-21hz-generator.pth"     --decoder-config-name firefly_gan_vq
